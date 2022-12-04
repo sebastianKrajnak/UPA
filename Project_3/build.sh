133ac7b30811d19cef6e9a74c784de1ebf40ce1f
sebastianKrajnak <sebastian.krajnak@gmail.com>
2022-12-04 23:48:44 +0100

    bugfix for merlin

--- Project_3/build.sh
+++ Project_3/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-python -m venv venv
+
+python3 -m venv venv
 source venv/bin/activate
 pip install bs4
 pip install pandas
--- Project_3/run.sh
+++ Project_3/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#source venv/bin/activate
+source venv/bin/activate
 python3 urls.py
 head -20 urls.txt | python3 data.py
